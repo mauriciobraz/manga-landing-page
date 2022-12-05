b1dd2cff24b8930c505c49eb5d173dbd553290f5
Maurício Braz Gonçalves <unbluew@gmail.com>
2022-12-05 17:57:22 -0400

    fix: outline button inheriting background color

--- src/components/Buttons/OutlineButton.tsx
+++ src/components/Buttons/OutlineButton.tsx
@@ -1,13 +1,14 @@
-import { Button, type ButtonProps } from '.';
+import { ButtonProps } from '.';
 
 /** Extends the Button component adding a outline style. */
 export const OutlineButton: React.FC<ButtonProps> = ({ label, ...props }) => {
   return (
-    <Button
+    <button
       {...props}
-      label={label}
-      className="border border-brand-400 bg-transparent hover:border-transparent hover:bg-brand-400"
-    />
+      className="rounded-md border border-brand-400 bg-transparent py-2 px-4 text-sm font-medium text-white hover:border-transparent hover:bg-brand-400 hover:brightness-90"
+    >
+      {label}
+    </button>
   );
 };
 
